3766a0e5369a955588e18520ce06227490e43a9e
Computery <77170025+C0mputery@users.noreply.github.com>
2023-11-26 22:32:06 -0500

    Improved Project Cleanliness
    Updated all emuns with ones decompiled from ILSpy.
    Moved all emuns from the tabg client into there own folder.
    Renamed the TABG project to the TABGCommunityServer project

--- TABGServer/Packet.cs
+++ TABGServer/Packet.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TABG
+namespace TABGCommunityServer
 {
     internal class Packet
     {
-        public ClientEventCode Type { get; set; }
+        public EventCode Type { get; set; }
         public byte[] Data { get; set; }
-        public Packet(ClientEventCode type, byte[] data)
+        public Packet(EventCode type, byte[] data)
         {
             Data = data;
             Type = type;
---
+++ TABGServer/TABGClientEmuns/ServerResponse.cs
@@ -0,0 +1,12 @@
+﻿// ServerResponse gotten from the TABG Client, on 11/23/2023, using ILspy
+[Flags]
+public enum ServerResponse : byte
+{
+    Error = 0,
+    Accepted = 1,
+    SquadDontFit = 2,
+    SquadIsBiggerThanTeamSize = 4,
+    DontHaveBooking = 8,
+    MatchAlreadyStarted = 0x10,
+    WrongPassword = 0x20
+}
